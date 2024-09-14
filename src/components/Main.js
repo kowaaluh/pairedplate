dc36753bfe933ed45a978bae58a697154b137d27
kowaaluh <kowaaluh24@gmail.com>
2024-09-14 01:11:33 -0600

    v1.0

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,9 +12,7 @@ Amplify.configure(awsExports);
 function Main() {
   const [restaurants, setRestaurants] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [modal, setModal] = useState(false);
   const [url, setUrl] = useState('');
-  const [disabled, setDisabled] = useState(false);
   const [search, setSearch] = useState('');
   const alert = useRef(null);
 
@@ -26,25 +24,8 @@ function Main() {
     }
   };
 
-  const scrollToSection = () => {
-    if (alert.current) {
-      alert.current.scrollIntoView({ behavior: 'auto' });
-    }
-  };
-
-  const handleClick = (url) => {
-    setModal(true);
-    setUrl(url);
-    setDisabled(true);
-    scrollToSection();
-  };
-
-  const closeModal = () => {
-    setModal(false);
-    setDisabled(false);
-  };
-
   const goToSite = () => {
+    setUrl(url);
     window.location.href = url;
   }
 
@@ -105,37 +86,10 @@ function Main() {
               <h3 className="mt-2 max-w-2xl text-center text-2xl font-bold leading-tight sm:text-3xl md:text-4xl md:leading-tight">Want to find tasty food that meets your dietary needs?</h3>
               <form action="" className="mx-auto mt-4 flex w-full max-w-md flex-col gap-3 sm:flex-row sm:gap-0">
                 <input type="search" name="search" onChange={inputChange} className="grow rounded border-2 border-green-950 py-3 px-3 focus:border-green-950 focus:outline-none sm:rounded-l-md sm:rounded-r-none sm:border-r-0" placeholder="Enter State" />
-                <button onClick={() => findFood()} type="button" disabled={disabled} className="rounded bg-green-950 px-5 py-4 font-bold text-white sm:rounded-l-none sm:rounded-r-md">Find Food</button>
+                <button onClick={() => findFood()} type="button" className="rounded bg-green-950 px-5 py-4 font-bold text-white sm:rounded-l-none sm:rounded-r-md">Find Food</button>
               </form>
             </div>
           </div>
-            {modal && (
-              <div className="max-w-lg mx-auto">
-                  <div className="m-4 flex items-center justify-center">
-                    <div className="w-96 rounded-xl p-5 border border-gray-200 bg-yellow-600">
-                        <p className="text-center text-white font-bold  text-lg"> You are about to leave this site. Are you sure you want to continue? </p>
-                        <div className="m-2 flex justify-center gap-4">
-                            <button onClick={() => closeModal()} className="bg-green-950 text-white font-bold py-2 px-4 rounded focus:outline-none">
-                              stay here
-                            </button>
-                            <button onClick={() => goToSite()} className="bg-green-950 text-white font-bold py-2 px-4 rounded focus:outline-none">
-                              continue
-                            </button>
-                        </div>
-                    </div>
-                  </div>
-                  <div className="flex items-center justify-center mb-4">
-                    <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
-                          <svg fill='none' className="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
-                              <path clipRule='evenodd'
-                                  d='M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z'
-                                  fill='currentColor' fillRule='evenodd' />
-                          </svg>
-                        <div>Loading ...</div>
-                    </div>
-                  </div>
-              </div>
-            )}
           <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
               {loading ? (
                 <div className="flex items-center justify-center w-full h-full">
@@ -159,7 +113,7 @@ function Main() {
                       <div>
                         <div className="text-center text-sm text-black font-bold mb-2">{restaurant.address}</div>
                       </div>
-                      <button disabled={disabled} onClick={() => handleClick(restaurant.website)} className="bg-green-950 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 focus:outline-none">
+                      <button onClick={() => goToSite(restaurant.website)} className="bg-green-950 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 focus:outline-none">
                         Website
                       </button>
                       </div>
@@ -191,9 +145,6 @@ function Main() {
                     <div class="text-xs uppercase text-gray-400 font-medium mb-6">
                         Community
                     </div>
-                    <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                        Instagram
-                    </a>
                     <a href="https://www.tiktok.com/@pairedplate" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
                         TikTok
                     </a>
