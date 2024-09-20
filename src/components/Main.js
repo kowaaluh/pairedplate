4b8c628b781213e21549a2cfb4b2c80e9f5fc5e5
kowaaluh <kowaaluh24@gmail.com>
2024-09-19 23:35:42 -0600

    search icon

--- src/components/Main.js
+++ src/components/Main.js
@@ -85,9 +85,9 @@ function Main() {
                           </span>
                         </div>
                         <h1 className="m-4 text-center font-bold text-3xl text-black">Get paired with foods that you can enjoy</h1>
-                            <div className="flex justify-center items-center">
+                            <div className="bg-white rounded flex justify-center items-center">
                                 <input
-                                    className="text-gray-400 outline-none p-2 focus:text-black"
+                                    className="rounded text-gray-400 outline-none p-2 focus:text-black w-full"
                                     type="search"
                                     name="search"
                                     onChange={inputChange}
@@ -95,10 +95,14 @@ function Main() {
                                 />
                                 <button
                                     onClick={() => findFood()}
-                                    className="bg-green-950 text-white rounded-r p-2"
+                                    className="bg-white rounded text-white p-2"
                                     type="button"
                                 >
-                                    Search
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                                width="16" height="16">
+                                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                                </svg>
                                 </button>
                             </div>
                     </form>
